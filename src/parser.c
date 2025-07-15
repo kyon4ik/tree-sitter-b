f70b53c48bc668046102dff2919572f826185c3b
kyon4ik <warriorkesh4@gmail.com>
2025-07-15 14:56:12 +0500

    use latest abi

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 153
 #define LARGE_STATE_COUNT 26
 #define SYMBOL_COUNT 76
@@ -1325,7 +1325,7 @@ static bool ts_lex_keywords(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 9},
   [2] = {.lex_state = 9},
@@ -6034,6 +6034,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_b(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -6053,6 +6054,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_b(void) {
     .keyword_lex_fn = ts_lex_keywords,
     .keyword_capture_token = sym_name,
     .primary_state_ids = ts_primary_state_ids,
+    .name = "b",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 0,
+      .minor_version = 1,
+      .patch_version = 0,
+    },
   };
   return &language;
 }
